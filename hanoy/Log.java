614d97c99fe3748abe4f6966316be97308c99929
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-05-15 17:45:01 +0200

    hanoy

---
+++ hanoy/Log.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Log {
+    public static void logger (List<Integer> x){
+        System.out.println(x);
+    }
+    public static void lognum(int x){
+        System.out.println(x);
+    }
+}
