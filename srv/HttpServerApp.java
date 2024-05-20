6fbf77e3b3333139b01101cf143c1a02a4f5c55b
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-05-20 09:57:01 +0200

    server

---
+++ srv/HttpServerApp.java
@@ -0,0 +1,32 @@
+import java.net.ServerSocket;
+import java.nio.charset.StandardCharsets;
+
+public class HttpServerApp {
+    public static void main(String[] args) throws Exception{
+        var serverSocket = new ServerSocket(8080);
+
+
+        while(true){
+            var connection  = serverSocket.accept();
+
+            try(var os = connection.getOutputStream()){
+                    var body = """
+                         {
+                         "id":"hello world"
+                          }
+                        """;
+
+                    var response = """
+                                    HTTP/1.1 200 OK
+                                    Content-Type: application/json
+                                    Content-Length: %d
+
+                                    %s
+                                   """.formatted(body.getBytes(StandardCharsets.UTF_8).length, body);
+
+                    os.write(response.getBytes(StandardCharsets.UTF_8));
+            }
+        }
+    }
+    
+}
